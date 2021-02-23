283bdbf9023559391eef78305a263bbbc942bfe5
cerceas <bulawancs@students.national-u.edu.ph>
2021-02-23 12:06:27 +0800

    Covid Update - Updates.
    based on my research i can't seems to find API for history cases specifically for asia. So I Just use API to get specifc data for a specific country to get all the history cases data from specific country that is under asia. Then I add all of data based from the date.
    chart finish.

--- lib/corona/covid_bar.dart
+++ lib/corona/covid_bar.dart
@@ -4,7 +4,6 @@ import 'package:charts_flutter/flutter.dart' as charts;
 import 'dart:convert';
 import "package:intl/intl.dart";
 import 'package:final_project/corona/data.dart' as globals;
-
 import 'model/covid_bar_chart.dart';
 
 class CovidBarChart extends StatefulWidget {
@@ -13,22 +12,31 @@ class CovidBarChart extends StatefulWidget {
 }
 
 class _CovidBarChartState extends State<CovidBarChart> {
+  var link1="https://disease.sh/v3/covid-19/historical/all?lastdays=220";
+  var link2="https://disease.sh/v3/covid-19/historical/Philippines?lastdays=all";
   List<charts.Series<TimeSeriesSales, DateTime>> seriesList;
   Future <Chart_Tcases> getJsonData() async{
-    var response = await http.get(
-      Uri.encodeFull(globals.chart_link),
+    var response = await http.get((globals.chart_link),
     );
 
     if(response.statusCode==200){
       var convertDataJson=null;
-      if(globals.chart_link=="https://disease.sh/v3/covid-19/historical/all?lastdays=all"){
+      if(globals.chart_link=="https://disease.sh/v3/covid-19/historical/all?lastdays=220"){
+
         convertDataJson = jsonDecode(response.body);
-      }else{
+        return Chart_Tcases.fromJson(convertDataJson);
+      }else if(globals.chart_link=="https://disease.sh/v3/covid-19/historical/Philippines?lastdays=all"){
+
         convertDataJson = jsonDecode(response.body)['timeline'];
+        return Chart_Tcases.fromJson(convertDataJson);
+      }else{
+        convertDataJson = jsonDecode(response.body);
+        return Chart_Tcases.fromJsonNew(convertDataJson);
       }
-      return Chart_Tcases.fromJson(convertDataJson);
+
     }else{
-      throw Exception("Check your internet connection or try again");
+        throw Exception("Check your internet connection or try again");
+
     }
   }
   generateData(days,days_val0,days_val1,days_val2,days_val3,days_val4,days_val5,days_val6) {
@@ -45,7 +53,7 @@ class _CovidBarChartState extends State<CovidBarChart> {
 
     var days5 = new DateTime(days.year, days.month- 1 , days.day);
 
-    var days6 = new DateTime(days.year, days.month, days.day);
+    var days6 = new DateTime(days.year, days.month, days.day-1);
 
 
 
@@ -74,6 +82,7 @@ class _CovidBarChartState extends State<CovidBarChart> {
     // TODO: implement initState
     super.initState();
     this.getJsonData();
+
     seriesList = List<charts.Series<TimeSeriesSales, DateTime>>();
   }
 
@@ -95,7 +104,7 @@ class _CovidBarChartState extends State<CovidBarChart> {
             padding: const EdgeInsets.all(20.0),
             alignment: Alignment.centerLeft,
             child: Text(
-              'Cases Statistics',
+              'Cases Bar-Chart based on JOHNS HOPKINS University',
               style: const TextStyle(
                 fontSize: 22.0,
                 fontWeight: FontWeight.bold,
@@ -112,50 +121,65 @@ class _CovidBarChartState extends State<CovidBarChart> {
                   if(condition%2!=0){
                     return CircularProgressIndicator();
                   }else {
-                    var covid = Snapshot.data;
+                    if(globals.chart_link==link1||globals.chart_link==link2){
+                      var covid = Snapshot.data;
+                      DateTime now = new DateTime.now();
+                      DateTime date = new DateTime(now.year, now.month, now.day);
+                      var days0 = new DateTime(date.year, date.month - 6, date.day);
 
-                    DateTime now = new DateTime.now();
-                    DateTime date = new DateTime(now.year, now.month, now.day);
-                    var days0 = new DateTime(
-                        date.year, date.month - 6, date.day);
+                      var formatter = new DateFormat('M/dd/yy');
+                      String date0 = formatter.format(days0);
 
-                    var formatter = new DateFormat('M/dd/yy');
-                    String date0 = formatter.format(days0);
+                      var days1 = new DateTime(
+                          date.year, date.month - 5, date.day);
+                      String date1 = formatter.format(days1);
 
-                    var days1 = new DateTime(
-                        date.year, date.month - 5, date.day);
-                    String date1 = formatter.format(days1);
+                      var days2 = new DateTime(
+                          date.year, date.month - 4, date.day);
+                      String date2 = formatter.format(days2);
 
-                    var days2 = new DateTime(
-                        date.year, date.month - 4, date.day);
-                    String date2 = formatter.format(days2);
+                      var days3 = new DateTime(
+                          date.year, date.month - 3, date.day);
+                      String date3 = formatter.format(days3);
 
-                    var days3 = new DateTime(
-                        date.year, date.month - 3, date.day);
-                    String date3 = formatter.format(days3);
+                      var days4 = new DateTime(
+                          date.year, date.month - 2, date.day);
+                      String date4 = formatter.format(days4);
 
-                    var days4 = new DateTime(
-                        date.year, date.month - 2, date.day);
-                    String date4 = formatter.format(days4);
+                      var days5 = new DateTime(
+                          date.year, date.month-1, date.day );
+                      String date5 = formatter.format(days5);
 
-                    var days5 = new DateTime(
-                        date.year, date.month - 1, date.day);
-                    String date5 = formatter.format(days5);
+                      var days6 = new DateTime(date.year, date.month, date.day-1);
+                      String date6 = formatter.format(days6);
 
-                    var days6 = new DateTime(date.year, date.month, date.day);
-                    String date6 = formatter.format(days6);
 
-                    print(date6);
-                    print(covid.cases['$date6']);
-                    generateData(
-                        date,
-                        covid.cases['$date0'],
-                        covid.cases['$date1'],
-                        covid.cases['$date2'],
-                        covid.cases['$date3'],
-                        covid.cases['$date4'],
-                        covid.cases['$date5'],
-                        covid.cases['$date6']);
+                      generateData(
+                          date,
+                          covid.cases['$date0'],
+                          covid.cases['$date1'],
+                          covid.cases['$date2'],
+                          covid.cases['$date3'],
+                          covid.cases['$date4'],
+                          covid.cases['$date5'],
+                          covid.cases['$date6']);
+                    }else{
+                      DateTime now = new DateTime.now();
+                      DateTime date = new DateTime(now.year, now.month, now.day);
+                      var covid = Snapshot.data;
+                      print(covid.data1);
+
+                      generateData(
+                          date,
+                          covid.data1,
+                          covid.data2,
+                          covid.data3,
+                          covid.data4,
+                          covid.data5,
+                          covid.data6,
+                          covid.data7,);
+                    }
+
 
                     return Flexible(
                       child: Row(
